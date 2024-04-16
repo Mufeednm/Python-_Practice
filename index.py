17488f53e7889b15dadcedf64737978c8f022b7d
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-16 16:16:08 +0530

    started python

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print("hey "  + input("what is your name ")+" "+ "how are u" )
+# print("hey "  + input("what is your name ")+" "+ "how are u" )
+          # Swap numbers
+# a= input("what is a s age")
+# b= input("what is  b s age")
+# temp=a
+# a=b
+# b=temp
+# print("a="+ a)
+# print ("b="+b)
