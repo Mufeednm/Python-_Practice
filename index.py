110d204dd9e310b6e5039f8daa94dfcb3788e0d4
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-17 13:02:07 +0530

    started python til operators

--- index.py
+++ index.py
@@ -7,3 +7,17 @@
 # b=temp
 # print("a="+ a)
 # print ("b="+b)
+#   #sum
+# a=input("type 2 digit numbers ")
+# firstnum= a[0]
+# secondnum=a[1]
+# sum =int( firstnum) + int(secondnum)
+# print(sum) 
+#     membership operator
+# a="mufeed"
+# print("u"not in a) 
+# print("u" in a) 
+
+
+
+
