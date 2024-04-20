1c5a9dcce479c996e3a5de9a8f1b8bd6e1a5d681
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-20 18:38:37 +0530

    studying more about  function in python

---
+++ function.py
@@ -0,0 +1,15 @@
+# def babu(a,b,c):
+#     sum =a+b+c
+#     avg=sum/3
+#     print(avg)
+#     # return avg
+# babu(1,2,
+a=int (input("enter a number"))
+def oddeven(x):
+    if x %2==0:
+        print("its even")
+    else:
+     print("its odd")
+        
+
+oddeven(a)
