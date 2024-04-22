6a1c92252c5a7b5af6e36b4e1a8a2fe4d37593dd
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-22 21:55:24 +0530

    studying about oops class artibute

--- Oops/Sample_class.py
+++ Oops/Sample_class.py
@@ -1,6 +1,14 @@
-class Car_Design:
-    pass
-Suv=Car_Design()
-Sedan= Car_Design()
-print(type(Suv))
-print(type(Sedan))
+# class Car_Design:
+#     pass
+# Suv=Car_Design()
+# Sedan= Car_Design()
+# print(type(Suv))
+# print(type(Sedan))
+#         adding detials or attributes
+class School:
+    def __init__(self,Name,age):
+        self.name=Name
+        self.age=age
+teacher=School("babu",50)
+pune = School("abu",25)         
+print(teacher.age)
