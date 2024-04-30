15dc000d4f2c77121788896f843ce50b69e58db2
mufeednm <mufeedmusthafanm@gmail.com>
2024-04-30 15:17:03 +0530

    studying about Decocartor

--- important topics/Decocartor.py
+++ important topics/Decocartor.py
@@ -1,14 +1,26 @@
-def magic (fun):
-    def change ():
+    #  sample method
+# def magic (fun):
+#     def change ():
       
-       return "manu"         
+#        return "manu"         
+#     return change
+
+# @magic
+# def name ():
+#    return "mufeed"
+# class a :
+#     pass
+# print(name())
+
+#    if function have arguments
+def magic(fun):
+    print ("magic")
+    def change (*args,**kwargs) :
+        result = fun( *args ,**kwargs  )
+        return result
     return change
-
-
-
 @magic
-def name ():
-   return "mufeed"
-class a :
-    pass
-print(name())
+def addition(a,b):
+    print("inside first function") 
+    return a+b
+print(addition(4,2))
